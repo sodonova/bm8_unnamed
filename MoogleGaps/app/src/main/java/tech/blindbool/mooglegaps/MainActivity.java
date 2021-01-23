4bdba253683be235e574f68a395a25d6c073af9f
aaryan-gautam <aaryanyo123@gmail.com>
2021-01-23 22:40:05 +0300

    Added buttons, user text and a basic layout for the MainActivity

--- MoogleGaps/app/src/main/java/tech/blindbool/mooglegaps/MainActivity.java
+++ MoogleGaps/app/src/main/java/tech/blindbool/mooglegaps/MainActivity.java
@@ -3,8 +3,11 @@ package tech.blindbool.mooglegaps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
